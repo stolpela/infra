69156aa456331c43b1184ef9a33795fa8fc8954a
stolpela <mail@stolpela.com>
2026-03-10 05:40:28 -0700

    updated flake again to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "=nix-k3s-cluster";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     sops-nix = {
