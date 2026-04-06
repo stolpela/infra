6aec055129c8a372dfedc17f3b71e02d09210d7f
stolpela <mail@stolpela.com>
2026-04-06 16:21:51 -0700

    added dns challenge

--- nix/modules/caddy.nix
+++ nix/modules/caddy.nix
@@ -2,34 +2,50 @@
 
 let
   cfg = config.caddy;
+
+  caddyCloudflare = pkgs.caddy.withPlugins {
+    plugins = [ "github.com/caddy-dns/cloudflare" ];
+    hash = lib.fakeHash;
+  };
 in
 {
   options.caddy = {
     enable = lib.mkEnableOption "Caddy reverse proxy";
 
+    cloudflareApiTokenFile = lib.mkOption {
+      type = lib.types.str;
+      default = "/etc/secrets/cloudflare_api_token";
+      description = "secret location";
+    };
+
     virtualHosts = lib.mkOption {
       type = lib.types.attrsOf (lib.types.submodule {
         options.reverseProxy = lib.mkOption {
           type = lib.types.str;
-          description = "Backend address to reverse proxy to (e.g. localhost:3000)";
+          description = "Backend address";
         };
       });
       default = {};
-      description = "Virtual hosts to configure as reverse proxies";
+      description = "hosts";
     };
   };
 
   config = lib.mkIf cfg.enable {
     services.caddy = {
       enable = true;
+      package = caddyCloudflare;
+
+      globalConfig = ''
+        acme_dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+      '';
 
       virtualHosts = lib.mapAttrs (domain: hostCfg: {
         extraConfig = "reverse_proxy ${hostCfg.reverseProxy}";
-      }) (lib.mapAttrs' (domain: hostCfg:
-        lib.nameValuePair "http://${domain}" hostCfg
-      ) cfg.virtualHosts);
+      }) cfg.virtualHosts;
     };
 
-    networking.firewall.allowedTCPPorts = [ 80 ];
+    systemd.services.caddy.serviceConfig.EnvironmentFile = cfg.cloudflareApiTokenFile;
+
+    networking.firewall.allowedTCPPorts = [ 80 443 ];
   };
 }
